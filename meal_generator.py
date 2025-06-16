57d6a8092b1019b0f9150be246631dda9898285d
Chase_S <58236767+chase-sabanski@users.noreply.github.com>
2025-06-16 12:09:03 -0500

    Update meal_generator.py

--- meal_generator.py
+++ meal_generator.py
@@ -1,28 +1,34 @@
-import random as rd
-
 proteins = ['chicken','salmon', 'pork','corvina', 'beef', 'trout', 'turkey', 'tilapia']
 carbs = ['yuca', 'sweet potato', 'rice', 'potato', 'amaranth', 'quinoa', 'wheat', 'sweet plantain', 'maiz', 'plantain (verde)']
 fruits = ['pear', 'banana', 'mandarin', 'grapes', 'mango', 'cooked apple', 'papaya', 'orange', 'granadilla', 'red fruits', 'pepino', 'coconut water', 'peach', 'cantelope', 'kiwi', 'watermelon', 'chirimoya']
 vegetables = ['avacado', 'broccoli', 'cooked carrot', 'tomato', 'cauliflower', 'zuccini', 'pepinillo']
 
-proteins_counter = 0
-carbs_counter = 0
-fruits_counter = 0
-vegetables_counter = 0
-day = 1
+counter = {
+    "protein": 0,
+    "carb": 0,
+    "fruit": 0,
+    "vegetable": 0,
+    "day": 1
+}
 
 def make_meal():
-    selected_protein = proteins[proteins_counter]
-    selected_carb = carbs[carbs_counter]
-    selected_fruit = fruits[fruits_counter]
-    selected_vegetable = vegetables[vegetables_counter]
     print(f"""
-    Day {day} lunch is...
-    Protein: {selected_protein}
-    Carb: {selected_carb}
-    Fruit: {selected_fruit}
-    Vegetable: {selected_vegetable}
+    Day {counter['day']} lunch is...
+    Protein: {proteins[counter['protein']]}
+    Carb: {carbs[counter['carb']]}
+    Fruit: {fruits[counter['fruit']]}
+    Vegetable: {vegetables[counter['vegetable']]}
 """
 )
 
-make_meal()
+for meal in range(30):
+    if counter['protein'] == len(proteins) - 1:
+        counter['protein'] = 0
+    if counter['carb'] == len(carbs) - 1:
+        counter['carb'] = 0
+    if counter['fruit'] == len(fruits) - 1:
+        counter['fruit'] = 0
+    if counter['vegetable'] == len(vegetables) - 1:
+        counter['vegetable'] = 0
+    make_meal()
+    counter = {k: v+1 for k,v in counter.items()}
