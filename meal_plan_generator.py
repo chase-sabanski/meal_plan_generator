880134bdb6cc14c689462538e049bddedb0ee651
Chase_S <58236767+chase-sabanski@users.noreply.github.com>
2025-06-20 19:09:52 -0500

    Update meal_plan_generator.py
    Adjusted the variety of fruit in a two day stretch. Consequently, it makes grocery shopping more complicated than I think it should be. But it was requested 🤷‍♂️

--- meal_plan_generator.py
+++ meal_plan_generator.py
@@ -28,9 +28,10 @@ def make_meal_plan():
     return(f"""Shopping list for days {counter['day']} and {counter['day'] + 1}:
 > Proteins: {proteins[counter['protein']]}, {proteins[counter['protein'] + 1]} 
 > Carbs: {carbs[counter['carb']]}, {carbs[counter['carb'] + 1]}
-> Fruits: {fruits[counter['fruit']]}, {fruits[counter['fruit'] + 1]}
+> Fruits: {fruits[counter['fruit']]}, {fruits[counter['fruit'] + 1]}, {fruits[counter['fruit'] + 2]}, {fruits[counter['fruit'] + 3]}
+    > For the oats, select one: strawberries/raspberries/blueberries/blackberries/uvillas/banana
 > Vegetables: {vegetables[counter['vegetable']]}, {vegetables[counter['vegetable'] + 1]}
-        
+
 Day {counter['day']} meals are...
 
     BREAKFAST
@@ -42,12 +43,12 @@ Day {counter['day']} meals are...
     -----------------------------------------
     Protein: {proteins[counter['protein']]}
     Carb: {carbs[counter['carb']]}
-    Fruit: {fruits[counter['fruit']]}
+    Fruit: {fruits[counter['fruit'] + 1]}
     Vegetable: {vegetables[counter['vegetable']]}
 
     SNACK
     -----------------------------------------
-    Fruit: {fruits[counter['fruit']]}
+    Fruit: strawberries/raspberries/blueberries/blackberries/uvillas/banana
 
     DINNER
     -----------------------------------------
@@ -61,18 +62,18 @@ Day {counter['day'] + 1} meals are...
     BREAKFAST
     -----------------------------------------
     Protein: boiled egg
-    Fruit: {fruits[counter['fruit'] + 1]}
+    Fruit: {fruits[counter['fruit'] + 2]}
 
     LUNCH
     -----------------------------------------
     Protein: {proteins[counter['protein'] + 1]}
     Carb: {carbs[counter['carb'] + 1]}
-    Fruit: {fruits[counter['fruit'] + 1]}
+    Fruit: {fruits[counter['fruit'] + 3]}
     Vegetable: {vegetables[counter['vegetable'] + 1]}
 
     SNACK
     -----------------------------------------
-    Fruit: {fruits[counter['fruit'] + 1]}
+    Fruit: strawberries/raspberries/blueberries/blackberries/uvillas/banana
 
     DINNER
     -----------------------------------------
@@ -91,6 +92,7 @@ meals = []
 for meal in range(15):
     meals.append(make_meal_plan())
     counter = {k: v+2 for k,v in counter.items()}
+    counter['fruit'] += 2
 
 # Changes the cwd to the directory of the file
 current_directory = os.path.dirname(__file__)
